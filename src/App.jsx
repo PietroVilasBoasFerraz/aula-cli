bd4c4f3c81861f95781f8029e1ee31ed0c9ec416
Pietro <pietrovilasboasferraz4@gmail.com>
2026-04-30 22:18:17 -0300

    inicia projeto copa

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+function App(){
+  return (
+    <div>
+      <header>World Cup 2026</header>
+        <nav>
+         <a href="#home">HOME</a>
+         <a href="#jogos">JOGOS</a>
+         <a href="#historia">A HISTÓRIA</a>
+        </nav>
+        <section id="home"></section>
+        <section id="jogos"></section>
+        <section id="historia"></section>
+      </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
